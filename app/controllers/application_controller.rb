ebef3c3560dad8b2ce1a9850d08cea50702cadcf
Tom Holmes <tholmes59@gmail.com>
2019-04-18 18:16:37 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,7 +19,8 @@ class ApplicationController < Sinatra::Base
     end
 
     def current_user
-      @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+      # @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+      User.find(session[:user_id])
     end
 
   end
